67ac060fac599a9f123a657fefc9aa25b589c998
TanmayLokhande <153291032+tanmaylokhande29@users.noreply.github.com>
2025-07-20 02:31:25 +0000

    Add conversion operations file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -84,8 +84,58 @@
 
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+/*------------------------------------------OPERATIONS-------------------------------------------------------------*/
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);  //power
+// console.log(2/3); 
+// console.log(10%3);  // reaminder
+
+
+
+                                        //adition of strings
+let str1 = " hello"
+let str2 = " Tanmay"
+console.log(str1+str2);
+// or
+
+let str3 = str1 +str2
+// console.log(str3);
+
+
+// console.log(1 +"2");    //12
+// console.log("1" + 2);   //12
+// console.log("1" +"2");   //12
+// console.log(1 + 1 +"2"); // 22 
+// console.log("1" + 1 +2);   //112
+
+
+// console.log((3 + 2) * 5 % 3 );   
+
+
+
+
+// should not be used
+console.log(true);  //true
+console.log(+true);  //1 when incremented true a "boolean" who works as a 1 get an incrementation
+console.log(+"");   //1
+
+
+let  numCounter = 100
+numCounter ++;
+console.log(numCounter);
+
