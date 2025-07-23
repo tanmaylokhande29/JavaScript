aa21f5a6e18cdb8b03b6af14f2438e168a2ba4b0
TanmayLokhande <153291032+tanmaylokhande29@users.noreply.github.com>
2025-07-23 03:26:54 +0000

    Add conversion operations file

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,4 +1,33 @@
 const score = 400
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
+
+// console.log(balance.toString().length);c
+// console.log(balance.toFixed(2));
+
+
+const constNumber = 123.8966
+// console.log(constNumber.toPrecision(3))
+
+const hundreds= 1000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+//************************************* Maths ************************************ */
+
+console.log(Math);
+
+console.log(Math.abs(-4)); //converting to absolute value
+console.log(Math.abs(4));
+
+
+
+console.log(Math.round(4.3));
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.2)); //5
+console.log(Math.floor(4.9)); //4
