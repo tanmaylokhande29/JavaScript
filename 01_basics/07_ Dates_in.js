a3d7a08ba91203ce22c054223c074c01b87d8354
TanmayLokhande <tanmaylokhande2964@gmail.com>
2025-07-26 23:51:35 +0530

    Add initial implementation for date operations

---
+++ 01_basics/07_ Dates_in.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toTimeString());
+
+console.log(typeof myDate);
