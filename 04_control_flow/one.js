a6122b76006faf428d46c5f5578aef02e6007204
TanmayLokhande <tanmaylokhande2964@gmail.com>
2025-09-11 16:35:36 +0530

    Add control flow examples and comments for user authentication and balance checks

---
+++ 04_control_flow/one.js
@@ -0,0 +1,63 @@
+//if
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature === 41){
+//   console.log("temperatur less than 50");
+// }
+// else{
+//   console.log("temperatur greater than 40");
+// }
+// console.log("execute");
+
+// const score = 200
+
+// if (score > 100) {
+//   const power = "fly"
+//   console.log( ` user power ${power}`);
+// }
+// console.log( ` user power ${power}`);
+
+const balance =1000
+// if (balance > 500) console.log("test"); /* implicit scope */
+
+// nested
+
+// if (balance < 500) {
+//   console.log("less than 500");
+
+// } else if (balance < 750) {
+//   console.log("less than 750 ");
+
+// } else if (balance < 900) {
+//   console.log("less than 900 ");
+
+// } else{
+//   console.log("less than 1200 ");
+
+// }
+
+const userLoggedIn = true
+const Debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+// if(userLoggedIn && Debitcard ){
+//   console.log("Allow user to buy course");
+
+// }
+
+if (loggedInFromGoogle || loggedInFromEmail ) {
+  console.log("logged in");
+
+}
+
+{/* < less then
+> greater than
+<= less than equal to
+>= greater than equal to
+== comparision
+!= not equal
+=== (strict equal) compares type too is 2==="2" then it says false as type is not same
+!== (strict not equal */}
+
