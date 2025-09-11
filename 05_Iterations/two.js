e9ea5567518bfa1dcf68dfc15b9c5f904310eaef
TanmayLokhande <tanmaylokhande2964@gmail.com>
2025-09-11 23:16:30 +0530

    while and do while

---
+++ 05_Iterations/two.js
@@ -0,0 +1,23 @@
+//while
+
+let index =0;
+while (index <= 10) {
+  // console.log(`index value is ${index}`);
+  index++;
+}
+
+let myArray = ['flash','ironman','superman']
+let arr =0
+while (arr <myArray.length) {
+  // console.log(`value is ${myArray[arr]}`);
+  arr++;
+}
+
+
+//do while loop
+let score =11
+do {
+  console.log(`Score is ${score}`);
+  score ++
+
+} while (score <=10);
