b714687acb00e92965642c35da9d92fccd50ba52
TanmayLokhande <tanmaylokhande2964@gmail.com>
2025-09-12 22:58:48 +0530

    Add implementation of for...in loop examples with objects, arrays, and maps

---
+++ 05_Iterations/four.js
@@ -0,0 +1,31 @@
+const myobject ={
+  js: 'javascript',
+  cpp: 'C++',
+  rb : 'ruby',
+  swift: 'swift by apple'
+}
+
+for (const key in myobject) {
+  // console.log(myobject[key]);
+  // console.log(`${key} shortcut is for ${myobject[key]}`)
+}
+
+
+const programming =["js","rb","py","java","cpp"]
+
+for(const key in programming){
+  // console.log(key);
+  // console.log(programming[key]);
+
+}
+
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United States of America')
+map.set('FR','France')
+map.set('IN','India')
+
+for (const key in map) {
+  console.log(key);
+ 
+}
